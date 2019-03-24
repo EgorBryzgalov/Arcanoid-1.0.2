b1ad4ed9a6163d358466c1dd9dd87e6fda61383d
Егор Брызгалов <egor@rus37.ru>
2019-03-24 12:57:10 +0300

    Ведется работа над логикой игры.

---
+++ Arcanoid 1.0.2/CExtends.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Arcanoid_1._0._2
+{
+    class CExtends
+    {// описывает размеры графического объекта и определяет столкновения
+        public int LeftX { get; set; }
+        public int RightX { get; set; }
+        public int UpperY { get; set; }
+        public int BottomY { get; set; }
+
+        public CExtends()
+        {
+
+        }
+        public CExtends(int X, int Y, int Width, int Heigh)
+        {
+            LeftX = X;
+            UpperY = Y;
+            RightX = X + Width;
+            BottomY = Y + Heigh;
+        }
+        
+    }
+}
---
+++ Arcanoid 1.0.2/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcanoid_1._0._2
+{
+    class Game
+    {
+
+
+    }
+}
